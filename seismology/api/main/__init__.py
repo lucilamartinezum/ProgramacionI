564f54778a6771e15f425c1b10c962d6da523976
lucilamartinezum <lucilamicaelamartinez@gmail.com>
2020-04-07 20:50:43 -0300

    fin de clase3

--- seismology/api/main/__init__.py
+++ seismology/api/main/__init__.py
@@ -2,14 +2,23 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-import main.resources as resources
+
 
 api = Api()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     load_dotenv()
+    if not os.path.exist(os.getenv('SQLALCHEMY_DATABASE_PATH')+os.getenv('SQLALCHEMY_DATABASE_NAME')):
+        os.mknod(os.getenv('SQLALCHEMY_DATABASE_PATH')+os.getenv('SQLALCHEMY_DATABASE_NAME'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('SQLALCHEMY_DATABASE_PATH')+os.getenv('SQLALCHEMY_DATABASE_NAME')
+    db.init_app(app)
+    import main.resources as resources
     api.add_resource(resources.SensorsResource, '/sensors')
     api.add_resource(resources.SensorResource, '/sensor/<id>')
     api.add_resource(resources.VerifiedseismsResource, '/verified-seisms')
